156dc7b3cbdde8d07fb014ebbec1fd0694f57039
robin <80066142+robinnlmn@users.noreply.github.com>
2021-12-10 16:50:37 +0100

    design page

--- pages/designs.js
+++ pages/designs.js
@@ -2,8 +2,58 @@ import React from 'react'
 
 export default function designs() {
     return (
-        <div>
-            DESIGNS
+        <div className="bg-black h-screen text-white">
+            <div className="bg-white shadow-xl dark:bg-111 flex items-center justify-center h-20">
+                <h1 className="text-black dark:text-white font-extrabold text-4xl">DESIGNS</h1>
+            </div>
+
+            <h1 className="text-black dark:text-white font-extrabold text-3xl p-6">Buttons</h1>
+            <div>
+                <div>
+                    <button className="m-6 p-2 bg-prired rounded-md shadow-prired hover:shadow-priredb transition duration-200">CLICK ME</button>
+                    <button className="m-6 p-2 border-prired border-4 rounded-md shadow-prired hover:shadow-priredb transition duration-200">CLICK ME</button>
+                </div>
+                <div>
+                    <button className="m-6 p-2 bg-priblue rounded-md shadow-priblue hover:shadow-priblueb transition duration-200">CLICK ME</button>
+                    <button className="m-6 p-2 border-priblue border-4 rounded-md shadow-pribluein hover:shadow-priblueinb transition duration-200">CLICK ME</button>
+                </div>
+            </div>
+
+            <h1 className="text-black dark:text-white font-extrabold text-3xl p-6">Cards</h1>
+            <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1">
+                <div className="m-6 p-2 border-purple-500 border-4 border-opacity-20 rounded-lg hover:border-opacity-50 transform rotate-2 hover:rotate-0 transition duration-500">
+                    <h1 className="font-bold text-2xl">This is a simple Card!</h1>
+                    <h1 className="font-bold text-xl pt-4 pb-2">Hover me!</h1>
+                </div>
+                <div className="m-6 p-2 border-purple-500 border-4 border-opacity-20 rounded-lg hover:border-opacity-50 transform -rotate-1 hover:rotate-0 transition duration-500">
+                    <h1 className="font-bold text-2xl">This is a simple Card!</h1>
+                    <h1 className="font-bold text-xl pt-4 pb-2">Hover me!</h1>
+                </div>
+                <div className="m-6 p-2 border-purple-500 border-4 border-opacity-20 rounded-lg hover:border-opacity-50 transform -rotate-3 hover:rotate-0 transition duration-500">
+                    <h1 className="font-bold text-2xl">This is a simple Card!</h1>
+                    <h1 className="font-bold text-xl pt-4 pb-2">Hover me!</h1>
+                </div>
+                <div className="m-6 p-2 border-purple-500 border-4 border-opacity-20 rounded-lg hover:border-opacity-50 transform rotate-6 hover:rotate-0 transition duration-500">
+                    <h1 className="font-bold text-2xl">This is a simple Card!</h1>
+                    <h1 className="font-bold text-xl pt-4 pb-2">Hover me!</h1>
+                </div>
+                <div className="m-6 p-2 border-purple-500 border-4 border-opacity-20 rounded-lg hover:border-opacity-50 transform -rotate-1 hover:rotate-0 transition duration-500">
+                    <h1 className="font-bold text-2xl">This is a simple Card!</h1>
+                    <h1 className="font-bold text-xl pt-4 pb-2">Hover me!</h1>
+                </div>
+                <div className="m-6 p-2 border-purple-500 border-4 border-opacity-20 rounded-lg hover:border-opacity-50 transform -rotate-3 hover:rotate-0 transition duration-500">
+                    <h1 className="font-bold text-2xl">This is a simple Card!</h1>
+                    <h1 className="font-bold text-xl pt-4 pb-2">Hover me!</h1>
+                </div>
+                <div className="m-6 p-2 border-purple-500 border-4 border-opacity-20 rounded-lg hover:border-opacity-50 transform -rotate-1 hover:rotate-0 transition duration-500">
+                    <h1 className="font-bold text-2xl">This is a simple Card!</h1>
+                    <h1 className="font-bold text-xl pt-4 pb-2">Hover me!</h1>
+                </div>
+                <div className="m-6 p-2 border-purple-500 border-4 border-opacity-20 rounded-lg hover:border-opacity-50 transform -rotate-3 hover:rotate-0 transition duration-500">
+                    <h1 className="font-bold text-2xl">This is a simple Card!</h1>
+                    <h1 className="font-bold text-xl pt-4 pb-2">Hover me!</h1>
+                </div>
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  //darkMode: 'media',
-  darkMode: false,
+  darkMode: 'media',
+  //darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '111': '#111',
+        '222': '#222',
+        'prired': '#FD4D4D',
+        'priblue': '#3D98FF',
+        'transparent': 'transparent',
+      },
+      boxShadow: {
+        'prired': '0px 0px 24px rgba(253, 77, 77, 0.75)',
+        'priredb': '0px 0px 32px rgba(253, 77, 77, 0.82)',
+        'priredin': 'inset 0px 0px 18px rgba(253, 77, 77, 0.75)',
+        'priredinb': 'inset 0px 0px 24px rgba(253, 77, 77, 0.82)',
+
+        'priblue': '0px 0px 24px rgba(61, 152, 255, 0.75)',
+        'priblueb': '0px 0px 32px rgba(61, 152, 255, 0.82)',
+        'pribluein': 'inset 0px 0px 18px rgba(61, 152, 255, 0.75)',
+        'priblueinb': 'inset 0px 0px 24px rgba(61, 152, 255, 0.82)',
+      }
+    },
   },
   variants: {
     extend: {},
